c161909ccfb96b19fdab9b5f066b43d86360a87e
sunriser <sunriser098@gmail.com>
2024-02-19 11:17:48 -0600

    Add links, specify language, limit news, add subject, control category.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # What is this project about?
 This app accesses news about a particular topic and sends them by email.
+ - Send news with link
+ - Show news only in English 
+ - Show only limited number of news
+ - Send news with Subject
+ - Dynamic control of Category to type of news
+ - Add to a 24/7 server to send up-to date news daily
--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import requests as rq
 import streamlit as st
 from send_email import send_email
 
+topic = "tesla"
 api_key = st.secrets.credentials.api_key
-url = "https://newsapi.org/v2/everything?q=tesla&from=2024-01-19&" \
-      "sortBy=publishedAt&apiKey=" + api_key
+url = "https://newsapi.org/v2/everything?" \
+      f"q={topic}&" \
+      "from=2024-01-19&sortBy=publishedAt&apiKey=" + api_key + "&language=en"
 
 # make a request
 request = rq.get(url)
@@ -14,10 +16,13 @@ content = request.json()
 
 body = " "
 # Access the article titles and description
-for article in content["articles"]:
+for article in content["articles"][:20]:
     if article["title"] is not None:
         if article["description"] is not None:
-            body = body + article["title"] + "\n" + article["description"] + 2*"\n"
+            body = "Subject: Today's news" + "\n" + body + article["title"] \
+                   + "\n" + article["description"] + "\n" + article["url"] \
+                   + 2*"\n"
+
 body = body.encode("utf-8")
 send_email(message=body)
 
